d6a6fa05485ee4926c06e31c64d320f7661a7b0d
tombest1 <caleb_akinola@yahoo.com>
2020-09-25 16:15:54 +0100

    compute

---
+++ loop.js
@@ -0,0 +1,18 @@
+// let a = 10;
+// a++;
+// a++;
+// console.log(a);
+
+// for(let i=0; i<= 200; i++){
+//     console.log(`this is number ${i}`);
+
+// this works
+
+// for(let i=0; i<= 10; i++){
+//     if(i%2==1)
+//    console.log(`this is number is odd ${i}`)
+// }   
+
+let myBtn = document.getElementsByClassName("mybtn");
+myBtn.getEventListener 
+
